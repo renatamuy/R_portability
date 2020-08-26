60f88e51f798a54cfb4829d42ba186fd6b884623
Renata Muylaert <renatamuy@gmail.com>
2020-08-27 10:14:54 +1200

    Update script_needed_packages.R

--- script_needed_packages.R
+++ script_needed_packages.R
@@ -1,5 +1,5 @@
 # Saving list of needed packages
 
-files_list <- dir("C:/Users/rdelaram/Documents/R/win-library/3.6/", pattern="")
+files_list <- dir("C:/Users/rdelaram/Documents/R/win-library/4.0/", pattern="")
 
 dput(as.character(files_list)) # copy that from the console and paste in next script
